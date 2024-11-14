28c5f3ce50301a9c08567c7183102f60c7bc501c
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-11-14 17:50:32 +0100

    Minor

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,11 @@ export default function Home() {
         <p className="text-md text-center text-muted-foreground md:px-24 md:text-lg xl:px-44 2xl:px-52">
           MediaShift is a{" "}
           <span className="text-primary">
-            free, self-hosted, no-limits, no-ads, no-anything
-          </span>
-          , no-BS file conversion service. It supports a wide range of file
-          formats and can convert them to a different format. It is a simple,
-          fast, and reliable service that can be used to convert files without
-          any hassle.
+            free, self-hosted, no-limits, no-ads, no-anything , no-BS
+          </span>{" "}
+          file conversion service. It supports a wide range of file formats and
+          can convert them to a different format. It is a simple, fast, and
+          reliable service that can be used to convert files without any hassle.
         </p>
       </div>
       <Dropzone />
